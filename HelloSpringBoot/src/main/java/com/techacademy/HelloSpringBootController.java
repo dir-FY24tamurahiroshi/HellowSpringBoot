f34dc5a28faa41e449cce889f4c083a34c803f46
HiroshiTamura(田村博) <HiroshiTamura()@Dirbato-1360x>
2024-04-24 12:03:26 +0900

    lesson19

--- HelloSpringBoot/src/main/java/com/techacademy/HelloSpringBootController.java
+++ HelloSpringBoot/src/main/java/com/techacademy/HelloSpringBootController.java
@@ -14,5 +14,16 @@ public class HelloSpringBootController {
     public String index() {
         return "Hello SpringBootの世界へ!";
     }
-    
+    @GetMapping("now")
+    public String dispTime() {
+        String now = LocalDateTime.now().format( DateTimeFormatter.ofPattern("HH:mm:ss") );
+
+        return "現在時刻：" + now;
+    }
+    @GetMapping("/plus/{val1}/{val2}")
+    public String calcPlus(@PathVariable int val1, @PathVariable int val2) {
+        int res = 0;
+        res = val1 + val2;
+        return "計算結果：" + res;
+    }
 }
